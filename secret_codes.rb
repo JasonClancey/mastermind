75ed7cbff371b0dfd71005d3fdb2e1126765d4a5
Jason Clancey <jason.clancey@dal.ca>
2022-09-12 19:59:47 -0600

    Reduced class variables to 1

--- secret_codes.rb
+++ secret_codes.rb
@@ -1,29 +1,30 @@
 class SecretCode
     require_relative 'display'
-    attr_reader :code_valid, :player_code, :computer_code
+    attr_reader :secret_code
     include Display
 
     def initialize
-        @player_code = ''
-        @computer_code = ''
+        @secret_code = nil
     end
 
-    def player_code
+    def choose_player_code
         display_request_player_input
-        @player_code = gets.chomp
+        @secret_code = gets.chomp
+        valid_code?
     end
 
-    def computer_code # random for now
-        @computer_code = Array.new(4) { rand(1..6) }
+    def choose_computer_code
+        #random for now
+        @secret_code = Array.new(4) { rand(1..6) }
     end
 
     def valid_code?
-        @player_code = @player_code.split(//).map { |digit| digit.to_i }
-        if @player_code.length == 4 && @player_code.all? { |digit| digit.instance_of?(Integer) && digit.between?(1,6) }
+        @secret_code = @secret_code.split(//).map { |digit| digit.to_i }
+        if @secret_code.length == 4 && @secret_code.all? { |digit| digit.instance_of?(Integer) && digit.between?(1,6) }
             display_code_is_valid
         else 
             display_code_is_invalid
-            self.player_code
+            self.choose_player_code
         end
     end
 end
